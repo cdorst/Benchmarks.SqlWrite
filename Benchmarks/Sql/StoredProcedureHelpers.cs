4c61a4336205c0514263c6ed8feed8b61531a5af
Christopher Dorst <chris@dorst.email>
2018-06-10 01:17:33 -0500

    Add StoredProcedureHelpers

---
+++ Benchmarks/Sql/StoredProcedureHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using static System.String;
+using static System.Byte;
+
+namespace Benchmarks.Sql
+{
+    internal static class StoredProcedureHelpers
+    {
+        private const string Space = " ";
+        private static readonly string Comma = "   ,";
+        private static readonly string NoComma = "    ";
+
+        public static string Parameters(in SqlParameter[] parameters = default)
+        {
+            if (parameters == null) return Empty;
+            var sb = new StringBuilder();
+            for (byte i = MinValue; i < parameters.Length; i++)
+            {
+                ref readonly var param = ref parameters[i];
+                sb.AppendLine(Concat(Indent(in i), param.Name, Space, param.Type));
+            }
+            return sb.ToString();
+            ref readonly string Indent(in byte index)
+            {
+                if (index == MinValue)
+                    return ref NoComma;
+                else
+                    return ref Comma;
+            }
+        }
+    }
+}
