7142c59a5788cc11e5bf21de1baf8c821b5f19cc
Christopher Dorst <chris@dorst.email>
2018-06-15 02:25:56 -0500

    Fix build: comment-out work-in-progress that should not have been committed w/o first committing the SqlParameter struct implementation

--- Benchmarks/Sql/StoredProcedureHelpers.cs
+++ Benchmarks/Sql/StoredProcedureHelpers.cs
@@ -10,23 +10,23 @@ namespace Benchmarks.Sql
         private static readonly string Comma = "   ,";
         private static readonly string NoComma = "    ";
 
-        public static string Parameters(in SqlParameter[] parameters = default)
-        {
-            if (parameters == null) return Empty;
-            var sb = new StringBuilder();
-            for (byte i = MinValue; i < parameters.Length; i++)
-            {
-                ref readonly var param = ref parameters[i];
-                sb.AppendLine(Concat(Indent(in i), param.Name, Space, param.Type));
-            }
-            return sb.ToString();
-            ref readonly string Indent(in byte index)
-            {
-                if (index == MinValue)
-                    return ref NoComma;
-                else
-                    return ref Comma;
-            }
-        }
+        //public static string Parameters(in SqlParameter[] parameters = default)
+        //{
+        //    if (parameters == null) return Empty;
+        //    var sb = new StringBuilder();
+        //    for (byte i = MinValue; i < parameters.Length; i++)
+        //    {
+        //        ref readonly var param = ref parameters[i];
+        //        sb.AppendLine(Concat(Indent(in i), param.Name, Space, param.Type));
+        //    }
+        //    return sb.ToString();
+        //    ref readonly string Indent(in byte index)
+        //    {
+        //        if (index == MinValue)
+        //            return ref NoComma;
+        //        else
+        //            return ref Comma;
+        //    }
+        //}
     }
 }
