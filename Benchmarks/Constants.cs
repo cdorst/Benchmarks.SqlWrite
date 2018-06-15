b280b906f3ca45a19263f08f55a7db534503dd3c
Christopher Dorst <chris@dorst.email>
2018-06-15 02:26:56 -0500

    Add PacketSize option to SQL connection string

--- Benchmarks/Constants.cs
+++ Benchmarks/Constants.cs
@@ -15,7 +15,7 @@ namespace Benchmarks
         public const string DatabaseDbName = "Benchmarks-53AB33DA-5FC4-4442-911B-DDEA265DAAB7";
         public const string DatabaseHostName = "localhost";
 
-        public static readonly string ConnectionString = string.Concat("Persist Security Info=False;Server=", DatabaseHostName,
+        public static readonly string ConnectionString = string.Concat("Persist Security Info=False;Packet Size=512;Server=", DatabaseHostName,
             ";User ID=", DatabaseUserName,
             ";Password=", DatabasePassword,
             ";Initial Catalog=", DatabaseDbName, ";");
