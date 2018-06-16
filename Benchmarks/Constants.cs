c6c280546b3fc2deb4e896bfbf4315c2b7a17f15
Christopher Dorst <chris@dorst.email>
2018-06-15 22:42:04 -0500

    Add CommandType.StoredProcedure to Constants

--- Benchmarks/Constants.cs
+++ Benchmarks/Constants.cs
@@ -1,4 +1,5 @@
 ﻿using Benchmarks.Model;
+using System.Data;
 
 namespace Benchmarks
 {
@@ -10,12 +11,14 @@ namespace Benchmarks
         public const string ExecCommand = "EXEC [dbo].[NotNC_AsInt] ";
         public const string NativeExecBytesCommand = "EXEC [dbo].[Is_NC_Bytes] ";
         public const string NativeExecCommand = "EXEC [dbo].[Is_NC_AsInt] ";
-        public static readonly EntityA Entity = new EntityA { EntityBId = 5, EntityCId = 5 };
 
         public const string DatabaseUserName = "SA";
         public const string DatabasePassword = "DevelopmentP@ssw0rd";
         public const string DatabaseDbName = "Benchmarks-53AB33DA-5FC4-4442-911B-DDEA265DAAB7";
         public const string DatabaseHostName = "localhost";
+
+        public static readonly EntityA Entity = new EntityA { EntityBId = 5, EntityCId = 5 };
+        public static readonly CommandType StoredProcedure = CommandType.StoredProcedure;
 
         public static readonly string ConnectionString = string.Concat("Persist Security Info=False;Packet Size=512;Server=", DatabaseHostName,
             ";User ID=", DatabaseUserName,
